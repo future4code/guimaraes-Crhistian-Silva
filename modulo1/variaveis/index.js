51157b9391d1aab806db54c86eac8e681570492f
crhistian <crhisnokia@live.com>
2021-11-09 11:07:15 -0300

    comecei a usar vscode

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,74 @@
+/* // Exemplo de Comentários
+
+// duas barras comenta uma linha (ctrl + ;)
+
+
+/* Os simbolos de asterisco barra (para abrir) e barra asterisco (para fechar) comenta todo o bloco que tiver dentro dele (Shift + Alt + A) */
+
+
+// Exemplo de console.log ()
+/* 
+console.log("olá mundo! ")
+console.log("Tudo o que tiver dentro do console.log vai aparecer para os desenvolvedores") */
+
+// Exemplo de prompt ()
+
+/* const nome = prompt("Qual é o seu nome ?")
+
+console.log(nome) */
+
+// Exemplo de declaração de variávies
+
+/* const idade = 89
+idade = 12
+console.log(idade) */
+
+/* let idade = 23
+idade = 38
+console.log(idade)
+
+
+const nomeCompleto = "Crhistian Felipe" 
+
+console.log("Olá! Meu nome é", nomeCompleto, "e minha idade é", idade, "anos") */
+
+/* const minhaIdade = "38"
+const idadeDaJu = "19" 
+
+const soma = minhaIdade + idadeDaJu
+console.log(soma) */
+
+/* let booleano = true
+booleano = false
+console.log(booleano)
+ */
+
+/* const primeiroNome = "crhistian"
+const sobrenome = "silva" 
+let idade = 38
+let estudante = true 
+
+let saldoBancario = 100
+const deposito = 300
+
+const resultado = saldoBancario + deposito 
+console.log("Olá! Meu nome é", primeiroNome, sobrenome, "e tenho", idade, "anos. É estudante?", estudante)
+
+console.log(typeof primeiroNome, typeof idade, typeof estudante, saldoBancario, resultado)
+ */
+
+const nome = prompt("Qual é o seu nome?")
+const idade = Number(prompt("Qual é a sua idade ?"))
+
+console.log(typeof nome, typeof idade)
+/* const numero = 43
+const numeroString = numero.toString()
+
+console.log(typeof numero, typeof numeroString)
+
+const string = "43"
+const stringNumero = Number(string)
+
+console.log(typeof string, typeof stringNumero)
+
+ */
