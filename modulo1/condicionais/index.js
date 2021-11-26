abe20fc1619c2d84a567f49637eae31a0ea1628e
crhistian <crhisnokia@live.com>
2021-11-26 18:02:43 -0300

    quase terminei o desafio..falta ainda conseguir fazer o preco internacional

--- modulo1/condicionais/index.js
+++ modulo1/condicionais/index.js
@@ -33,9 +33,9 @@ else{
 }
 
 /* 2. Agora faça um programa que verifica que turno do dia um aluno estuda. Peça para digitar M (matutino) ou V (Vespertino) ou N (Noturno).
- Imprima no console a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!". Utilize o bloco if/else
-  */
- const turnoaluno = prompt("Qual turno você estuda ? Responda usando as letras  M se for (matutino) ou V se for (Vespertino) ou N se for (Noturno)").toUpperCase()
+ Imprima no console a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!". Utilize o bloco if/else */
+
+const turnoaluno = prompt("Qual turno você estuda ? Responda usando as letras  M se for (matutino) ou V se for (Vespertino) ou N se for (Noturno)").toUpperCase()
 
  if (turnoaluno === "M"){
   console.log("Bom Dia!")
@@ -52,9 +52,9 @@ else{
 
 
 //3. Repita o exercício anterior, mas utilizando a estrutura de switch case agora.
-const turnoaluno = prompt("Qual turno você estuda ? Responda usando as letras  M se for (matutino) ou V se for (Vespertino) ou N se for (Noturno)").toUpperCase()
+const turnoaluno2 = prompt("Qual turno você estuda ? Responda usando as letras  M se for (matutino) ou V se for (Vespertino) ou N se for (Noturno)").toUpperCase()
  
-switch (turnoaluno){
+switch (turnoaluno2){
     case "M":
          console.log("Bom dia")
          break
@@ -68,12 +68,12 @@ switch (turnoaluno){
          console.log("Você não inseriu a opção correta, por favor corrija seus dados")
          break
  }
-
+ 
  /* 4. Considere a situação: você vai ao cinema com um amigo ou amiga, porém ele/ela só assistirá a um filme com você se ele
  for do gênero fantasia e se o ingresso está abaixo de 15 reais. Faça um código que pergunta ao usuário qual o gênero de filme 
  que vão assistir e outra pergunta sobre o preço do ingresso, então verifique se seu amigo ou amiga vai topar assistir o filme.
   Caso positivo, imprima no console a mensagem: "Bom filme!", caso contrário, imprima "Escolha outro filme :( "
- */
+ */ 
 const generoFilme = prompt("Qual é o gênero do filme que você vai assitir ?").toLowerCase()
 const precoIngresso = Number(prompt("Qual o valor do ingresso"))
 if(generoFilme === "fantasia" && precoIngresso < 15){
@@ -86,10 +86,9 @@ if(generoFilme === "fantasia" && precoIngresso < 15){
  // DESAFIOS
  /* 1. Modifique o código do exercício 4 de escrita de código para, antes de imprimir a mensagem "Bom filme!",
  pergunte ao usuário, pelo prompt qual lanchinho ele vai comprar (pipoca, chocolate, doces, etc) e imprima no console
-  as mensagens "Bom filme!" e "Aproveite o seu [LANCHINHO]", trocando [LANCHINHO] pelo que o usuário colocou no input.
- */
+  as mensagens "Bom filme!" e "Aproveite o seu [LANCHINHO]", trocando [LANCHINHO] pelo que o usuário colocou no input*/
 
-const generoFilme = (nomeFilme, precoIngresso) =>{
+const generoFilme1 = (nomeFilme, precoIngresso) =>{
     if(nomeFilme === "fantasia" && precoIngresso < 15){
         const lanchinho = prompt("Qual lanchinho você vai comprar? ")
         console.log(`Bom filme ! Aproveite o seu ${lanchinho}`)
@@ -102,6 +101,85 @@ const generoFilme = (nomeFilme, precoIngresso) =>{
 let generoFilmeUser = prompt("Qual é o gênero do filme que você vai assitir ?").toLowerCase()
 let valorIngressoUser = Number(prompt("Qual o valor do ingresso"))
 
-generoFilme(generoFilmeUser, valorIngressoUser)
+generoFilme1(generoFilmeUser, valorIngressoUser)
 
+ /* 2. Você foi contratado para criar um sistema de vendas de ingressos de jogos de um estádio de futebol. 
+Para esta compra, o usuário deve fornecer algumas informações:
+    - Nome completo;
+    - Tipo de jogo: IN indica internacional; e DO indica doméstico;
+    - Etapa do jogo: SF indica semi-final; DT indica decisão de terceiro lugar; e FI indica final
+    - Categoria: pode ser as opções 1, 2, 3 ou 4;
+    - Quantidade de ingressos
+    
+    O seu sistema deve solicitar estas informações ao usuário, através do `prompt` . 
+    Além disso, ele deve imprimir tudo isso, junto com o valor de cada ingresso e o valor total que o usuário
+     tem que pagar (ou seja, o valor unitário do ingresso multiplicado pela quantidade).
+      Abaixo, há a tabela com os valores de cada ingresso e exemplos de execução do programa. 
+      Lembrando que o valor de jogos internacionais é o mesmo de jogos domésticos, mas seus preços devem ser
+       multiplicados pelo valor do dólar (considerar o dólar = R$4,10) */
+let nomeComprador = prompt(" Olá, por favor digite seu nome completo")
+let tipoJogoUsuario = prompt("Qual tipo de jogo você está buscando ingressos: **ATENÇÃO** use **DI** para jogo Internacional, e **DO** para jogo doméstico").toUpperCase()
+let etapaJogoUsuario = prompt("Qual estapa você quer assistir: **ATENÇÃO** use **SF para semi-final, **DT** para decisão de terceiro lugar, e **FI** para a grande final").toUpperCase()
+let categoriaJogo = Number(prompt("Digite qual categoria de ingresso está buscando: **ATENÇÃO** use: 1, 2, 3, 4"))
+let quantIngresso = Number(prompt("Digite a quantidade de ingressos"))
+   
+const funcaoValorPago = (tipo, etapa, categoria) => {
+  let precoUnitario;
+  if(tipo === "DO" && etapa === "SF" && categoria === 1 ){
+    precoUnitario = 1320.00}
+    if(etapa ==="SF" && categoria === 2){
+      precoUnitario = 880.00}
+      if(etapa === "SF" && categoria === 3){
+        precoUnitario = 550.00}
+        if(etapa ==="SF" && categoria === 4){
+          precoUnitario = 220.00}
+          else if(tipo === "DT" && categoria === 1 ){
+            precoUnitario = 660.00}
+  if(etapa ==="DT" && categoria === 2){
+    precoUnitario = 440.00}
+    if(etapa === "DT" && categoria === 3){
+      precoUnitario = 330.00}
+      if(etapa ==="DT" && categoria === 4){
+        precoUnitario = 170.00}
+        else if(etapa === "FI" && categoria === 1 ){
+          precoUnitario = 1980.00}
+  if(etapa ==="FI" && categoria === 2){
+    precoUnitario = 1320.00}
+    if(etapa === "FI" && categoria === 3){
+      precoUnitario = 880.00}
+      if(etapa ==="FI" && categoria === 4){
+        precoUnitario = 330.00
+    }
+return precoUnitario
+}
 
+let etapaJogo;
+if(etapaJogoUsuario === "FI"){
+  etapaJogo = "GRANDE FINAL"
+}
+else if(etapaJogoUsuario === "DT"){
+  etapaJogo = "DECISÃO DE 3° LUGAR"
+} 
+else if(etapaJogoUsuario === "SF"){
+  etapaJogo = "SEMI-FINAL"
+}
+
+let tipoJogo;
+if(tipoJogoUsuario === "DO"){
+  tipoJogo = "DOMÉSTICO"
+}
+else if(tipoJogoUsuario	=== "DI"){
+  tipoJogo = "INTERNACIONAL"
+}
+
+let precoFinal = (funcaoValorPago(tipoJogoUsuario, etapaJogoUsuario, categoriaJogo) * quantIngresso)
+
+console.log( ` --- Dados da compra ---
+Nome do cliente: ${nomeComprador}
+Tipo do Jogo: ${tipoJogo}
+Etapa do jogo: ${etapaJogo}
+Categoria: ${categoriaJogo}
+Quantidade de Ingressos: ${quantIngresso} ingressos
+--- VALORES ---
+Valor do ingresso: R$ ${funcaoValorPago(tipoJogoUsuario, etapaJogoUsuario, categoriaJogo)} 
+Valor total: R$ ${precoFinal}`)
