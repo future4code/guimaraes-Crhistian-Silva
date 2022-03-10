f44ddd39f96d24413f2b3e251da6c21646030292
crhistian <crhisnokia@live.com>
2022-03-09 21:45:15 -0300

    restaurando arquivos

---
+++ modulo3/projeto-labefy/src/components/button.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './button.css'
+
+
+const ButtonEstilizado = () => {
+  return (
+    <button>
+    <span>Button</span>
+    <div class="liquid"></div>
+  </button>
+  )
+}
+
+export default ButtonEstilizado
