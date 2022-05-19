866ebbb1fa676d297437ddbc9ea40ccd74ef5faf
_crhis_ <crhisnokia@live.com>
2022-05-19 15:21:59 -0300

    aula inicial

--- modulo5/arquivos_Modulo_5/aula_inicial/index.js
+++ modulo5/arquivos_Modulo_5/aula_inicial/index.js
@@ -1,3 +1,5 @@
 import  countries from './countries.js'
 
-console.table(countries)
+const string = process.argv[2].toLowerCase()
+const resultado = countries.filter(pais=> pais.name.toLowerCase().includes(string))
+   console.table(resultado)
