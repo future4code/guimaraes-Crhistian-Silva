02c0c88b1022b5781cbbeacb4667f3b924ca3fdb
_crhis_ <crhisnokia@live.com>
2022-05-31 11:24:40 -0300

    resolucao

--- modulo5/node-package-json/exercicio1/index.js
+++ modulo5/node-package-json/exercicio1/index.js
@@ -1,30 +1,52 @@
-//Exercício 1//
+ //Exercício 1//
 //a) process.argv[2] (3,4,5.....)
 //b) 
-const yellow = '\x1b[33m%s\x1b[0m'
+/* const yellow = '\x1b[33m%s\x1b[0m'
 const green = '\x1b[32m%s\x1b[0m'
 const red = '\x1b[31m%s\x1b[0m'
-const purple = '\x1b[35m%s\x1b[0m'
+const purple = '\x1b[35m%s\x1b[0m' */
 
+const colors = require('colors') // colors é um pacote npm, instale-o com npm i colors
 
 const name = process.argv[2]
 const age = Number(process.argv[3])
-const number = Number(process.argv[4]) 
-const futureAge = (num) => age + num
 
- console.log(green, `Olá ${name} você tem ${age}` )
+ console.log(`Olá ${name} você tem ${age}`.green )
 
-console.log(`Olá, ${name}, Você tem  anos ${age}. Em sete anos você terá ${futureAge(number)}`) 
+console.log(`Olá, ${name}, Você tem  anos ${age}. Em sete anos você terá ${age + 7}`) 
 
 //outro médoto// 
 
-const showData = (name, age, number) => {
+const showData = (name, age) => {
     if(name && age){
-        console.log(green, `Olá ${name} Você tem ${age} anos`)
-    }if(name && age && number){
-        console.log(purple,`Olá, ${name}, Você tem  anos ${age}. Em sete anos você terá ${futureAge(number)}`)
+        console.log( `Olá ${name} Você tem ${age} anos`.red)
+        console.log(`Olá, ${name}, Você tem  anos ${age}. Em sete anos você terá ${age + 7}`.yellow)
+  
     }else{
         console.log('Está faltando algum parâmetro, confirme sua digitação')
     }
 }
-showData(name, age, number)
+showData(name, age) 
+
+//outro médoto// 
+
+/* 
+const showData = (name, idade,) => {
+
+    const colors = require('colors') // colors é um pacote npm, instale-o com npm i colors
+
+    const username = process.argv[2] // "name" é uma palavra reservada do sistema
+    const age = Number(process.argv[3])
+
+    const params = process.argv.length - 2
+    if (params < 2) {
+        return console.log(`Was expecting 2 parameters, got ${params}`.red)
+    }
+    console.log(`Olá, ${username}! Você tem ${age} anos.`.blue)
+
+    console.log(`Olá, ${username}! Você tem ${age} anos. Em sete anos você terá ${age + 7}`.green)
+}
+
+
+showData('crhistian', 38) */
+
