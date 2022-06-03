24f7b3fa8000cff2bfc7d8b53401a2b446c2f62f
_crhis_ <crhisnokia@live.com>
2022-06-03 00:26:20 -0300

    esqueci de mudar de branch

--- modulo5/aprofundamento-express/src/exe1.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-import { app } from "./index";
-
-app.get("/ping", (req:Request, res:Response) => {          
-    res.send("Pong! 🏓")
-})
--- modulo5/aprofundamento-express/src/exe2.ts
+++
@@ -1,6 +0,0 @@
-export type typeToDo ={
-    userId: number, 
-    id: number,
-    title: string,
-    completed: boolean
-}
