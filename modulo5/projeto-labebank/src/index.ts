f3228cc9a6f6b68df54030df4cf023aa7ac5481a
_crhis_ <crhisnokia@live.com>
2022-06-10 23:27:49 -0300

    exercicio pagamento feito

--- modulo5/projeto-labebank/src/index.ts
+++ modulo5/projeto-labebank/src/index.ts
@@ -154,7 +154,7 @@ app.get("/user/balance/:name", (req: Request, res: Response) => {
     {
       throw new Error(messageStatus.FORBIDDEN.message);
     }
-    //verificação se falta algum dado no body da requisição tipada como Users
+    //verificação se falta algum enviado nos Parâmetros
     if (!userName || !userCpf) 
     {
       throw new Error(messageStatus.MISSING_PARAMETERS.message);
@@ -166,7 +166,7 @@ app.get("/user/balance/:name", (req: Request, res: Response) => {
       throw new Error(messageStatus.USERS_NOT_FOUND.message);
     } 
     
-    const  balance = user.balance as number
+    const balance = user.balance as number
     
     const newBalance = balance.toFixed(2).split(".")
     newBalance[0] = newBalance[0].split(/(?=(?:...)*$)/).join(".")
@@ -198,6 +198,152 @@ app.get("/user/balance/:name", (req: Request, res: Response) => {
     }
   }
 });
+
+app.patch("/user/balance/credit/:name", (req:Request, res:Response) =>{
+
+  try {
+
+    const userAuthorization = req.headers.authorization as string
+    const userData = req.body
+    let newDescription = userData.description.toUpperCase()
+
+
+    if (!userAuthorization || userAuthorization.toUpperCase() !== authorization.toUpperCase() ) 
+    {
+      throw new Error(messageStatus.FORBIDDEN.message);
+    }
+    //verificação se falta algum enviado nos Parâmetros
+    if (!userData.name || !userData.cpf || !userData.value || ! userData.description || newDescription !== TRANSACTIONS.CREDIT ) 
+    {
+      throw new Error(messageStatus.MISSING_PARAMETERS.message);
+    }
+    
+    const userFind = usersLabebank.find((user)=> user.name.toUpperCase() === userData.name.toUpperCase() && user.cpf === userData.cpf)
+
+    if (!userFind) {
+      throw new Error(messageStatus.USERS_NOT_FOUND.message);
+    } 
+const date = new Date();
+const  newDate = String(date.getDate()).padStart(2, '0') + '/' + String(date.getMonth() + 1).padStart(2, '0') + '/' + date.getFullYear(); 
+usersLabebank.forEach((user:any)=>{
+  if(user.userId === userFind.userId ){
+    user.balance = user.balance + userData.value
+    user.extract = [... user.extract, {value: userData.value,
+    date: newDate,
+  description: newDescription} ]
+  }
+  return user
+})
+ 
+res.status(messageStatus.SUCCESS.status).send(messageStatus.SUCCESS.message)
+    
+  } catch (error: any) {
+    switch (error.message) {
+      case messageStatus.FORBIDDEN.message:
+        res
+          .status(messageStatus.FORBIDDEN.status)
+          .send(messageStatus.FORBIDDEN.message);
+        break;
+        case messageStatus.MISSING_PARAMETERS.message:
+          res
+            .status(messageStatus.MISSING_PARAMETERS.status)
+            .send(messageStatus.MISSING_PARAMETERS.message);
+          break;
+      case messageStatus.NOT_FOUND.message:
+        res
+          .status(messageStatus.NOT_FOUND.status)
+          .send(messageStatus.NOT_FOUND.message);
+        break;
+      default:
+        res
+          .status(messageStatus.SOME_ERROR.status)
+          .send(messageStatus.SOME_ERROR.message);
+    }
+  }
+});
+
+app.put("/user/balance/debit/:name", (req:Request, res:Response) =>{
+
+  try {
+
+    const userAuthorization = req.headers.authorization as string
+    const userData = req.body
+    let newDescription = userData.description.toUpperCase()
+
+    if (!userAuthorization || userAuthorization.toUpperCase() !== authorization.toUpperCase() ) 
+    {
+      throw new Error(messageStatus.FORBIDDEN.message);
+    }
+    //verificação se falta algum enviado nos Parâmetros
+    if (!userData.name || !userData.cpf || !userData.value || ! userData.description || newDescription !== TRANSACTIONS.DEBIT ) 
+    {
+      throw new Error(messageStatus.MISSING_PARAMETERS.message);
+    }
+    
+    const userFind = usersLabebank.find((user)=> user.name.toUpperCase() === userData.name.toUpperCase() && user.cpf === userData.cpf)
+
+    if (!userFind) 
+    {
+      throw new Error(messageStatus.NOT_FOUND.message);
+    }
+
+    if(userFind){
+      if((Number(userFind.balance)- Number(userData.value)) < 0){
+
+         throw new Error(messageStatus.NO_CONTENT.message);
+      }else{
+        const date = new Date();
+        const  newDate = String(date.getDate()).padStart(2, '0') + '/' + String(date.getMonth() + 1).padStart(2, '0') + '/' + date.getFullYear(); 
+        usersLabebank.forEach((user:any)=>{
+          if(user.userId === userFind.userId ){
+            user.balance = user.balance - userData.value
+            user.extract = [... user.extract, {value: userData.value,
+            date: newDate,
+          description: newDescription} ]
+          }
+          return user
+        })        
+        res.status(messageStatus.SUCCESS.status).send(messageStatus.SUCCESS.message)
+        return
+      }
+    } 
+
+ 
+res.status(messageStatus.SUCCESS.status).send(messageStatus.SUCCESS.message)
+    
+  } catch (error: any) {
+    switch (error.message) {
+      case messageStatus.FORBIDDEN.message:
+        res
+          .status(messageStatus.FORBIDDEN.status)
+          .send(messageStatus.FORBIDDEN.message);
+        break;
+        case messageStatus.MISSING_PARAMETERS.message:
+          res
+            .status(messageStatus.MISSING_PARAMETERS.status)
+            .send(messageStatus.MISSING_PARAMETERS.message);
+          break;
+      case messageStatus.NOT_FOUND.message:
+        res
+          .status(messageStatus.NOT_FOUND.status)
+          .send(messageStatus.NOT_FOUND.message);
+        break;
+      case messageStatus.NO_CONTENT.message:
+          res
+            .status(messageStatus.NO_CONTENT.status)
+            .send(messageStatus.NO_CONTENT.message);
+          break;
+      default:
+        res
+          .status(messageStatus.SOME_ERROR.status)
+          .send(messageStatus.SOME_ERROR.message);
+    }
+  }
+});
+
+
+
+
 
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
